242f0ae7cf26c3edf4570d9a1d838f36cf2ffa65
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-20 22:26:18 +0900

    :memo:  chore: make drawing board as canvas practice

--- app.js
+++ app.js
@@ -2,30 +2,30 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-  '#55efc4',
-  '#ffeaa7',
-  '#74b9ff',
-  '#a29bfe',
-  '#d63031',
-  '#00b894',
-  '#00cec9',
-  '#fdcb6e',
-  '#6c5ce7',
-  '#ff7675',
-]
-ctx.lineWidth = 2
-const position = Math.floor(Math.random() * 800)
+const onMove = (e) => {
+  if(isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.stroke()
+    return
+  }
 
-const mouseMove = (e) => {
-  const color = colors[Math.trunc(Math.random() * colors.length)]
+  ctx.moveTo(e.offsetX, e.offsetY);
+};
 
-  ctx.beginPath()
-  ctx.strokeStyle = color
-  ctx.moveTo(position, position)
-  ctx.lineTo(e.offsetX, e.offsetY)
-  ctx.stroke()
+const startPainting = () => {
+  isPainting = true;
+};
+
+const cancelPainting = () => {
+  isPainting = false
 }
 
-canvas.addEventListener("mousemove", mouseMove)
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+
+// document.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
