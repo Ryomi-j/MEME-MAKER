83f346d8f3117a1ef524dbfe76f57b29e619e00c
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-20 22:07:59 +0900

    :memo:  chore: practice to use mouse move event

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+const colors = [
+  '#55efc4',
+  '#ffeaa7',
+  '#74b9ff',
+  '#a29bfe',
+  '#d63031',
+  '#00b894',
+  '#00cec9',
+  '#fdcb6e',
+  '#6c5ce7',
+  '#ff7675',
+]
+ctx.lineWidth = 2
+const position = Math.floor(Math.random() * 800)
 
-ctx.arc(250, 100, 50, 0, 2*Math.PI)
-ctx.fill()
+const mouseMove = (e) => {
+  const color = colors[Math.trunc(Math.random() * colors.length)]
 
-ctx.beginPath()
-ctx.fillStyle = 'white'
-ctx.arc(270, 90, 8, Math.PI, 2 * Math.PI)
-ctx.arc(230, 90, 8, Math.PI, 2 * Math.PI)
-ctx.moveTo(250, 120)
-ctx.arc(250, 120, 8, 0, Math.PI)
-ctx.fill()
+  ctx.beginPath()
+  ctx.strokeStyle = color
+  ctx.moveTo(position, position)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.stroke()
+}
+
+canvas.addEventListener("mousemove", mouseMove)
